bb3c289bc2b9754e3c1ca8a31df1d656e3709013
GrantDalton <Grantdalton2003@gmail.com>
2021-03-16 08:27:58 -0400

    yay

--- Crab.java
+++ Crab.java
@@ -7,26 +7,50 @@ public class Crab extends Actor
 {
     public void act()
     {
-        if( isAtEdge( ) )
-        {
-            turn(17);
-        }
-         if ( Greenfoot.getRandomNumber(100) < 10 ) 
-        {
-          turn ( Greenfoot.getRandomNumber (91)-45);
-        }
+          turnAtEdge();
+          randomTurn();
           move();
-
-        if (isTouching(Worm.class))
-        {
-            removeTouching(Worm.class);
-        }
+          LookForWorm();
+       
     }
     
     public void move( )
     {
         move(5);
     }
+    
+    /*
+     * worm code for eating and finding worms
+     */
+    public void LookForWorm()
+    {
+         if (isTouching(Worm.class))
+        {
+            removeTouching(Worm.class);
+        }
+    }
+    
+    /*
+     * make the crab turn randomly between 0 and 45 degrees 
+     */
+    public void randomTurn() 
+    {
+         if ( Greenfoot.getRandomNumber(100) < 10 ) 
+            {
+              turn ( Greenfoot.getRandomNumber (91)-45);
+            }
+    }  
+    
+    /*
+     * turn at edge
+     */
+    public void turnAtEdge()
+    {
+        if( isAtEdge( ) )
+        {
+            turn(17);
+        }
+    }
 }
 
-
+ 
